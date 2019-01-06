e78b724492f4e3e22aa2e8bd229479b0e561c028
fuzy <xjtumsu@gmail.com>
2019-01-06 11:27:46 +0800

    add l1 norm constraint optim, no debug

--- mrtrpo_mpi/optim.py
+++ mrtrpo_mpi/optim.py
@@ -10,8 +10,10 @@ def get_coefficient(G,S,method = 1):
     H = np.dot(S, G.T)
     SS = np.dot(S, S.T)
     if method == 1:
+        return l1_norm_constraint(H,num_reward)
+    elif mothod == 2:
         return l2_norm_constraint(H,num_reward)
-    elif method == 2:
+    elif method == 3:
         return max_update(SS,H,num_reward)
     else:
         return coe/num_reward
@@ -37,6 +39,26 @@ def l2_norm_constraint(H,num_reward):
     f_cd, v_cd = qcqp.improve(COORD_DESCENT)
     #primal_result = prob.solve()
     return np.array(x.value).reshape((num_reward))
+def l1_norm_constraint(H,num_reward):
+	vals, vecs = np.linalg.eigh(H)
+    x = vecs[:,1]
+    for i in range(100):
+        index1 = np.argmax(x)
+        x[index1] = 0
+        index2 = np.argmax(x)
+        x[index2] = 0
+        lower = 0
+        upper = 1 - np.sum(x)
+        a = H[index1,index1] + H[index2,index2] - H[index1,index2] - H[index2,index1]
+        b = upper * (-2 * H[index2,index2] + H[index1,index2] + H[index2,index1])
+        middle = b/(-2 * a)
+        if middle < upper/2:
+        	op = upper
+        else
+        	op = 0
+        x[index1] = op
+        x[index2] = upper - op
+    return x
 
 def max_update(SS,H,num_reward):
     x = Variable(num_reward)
