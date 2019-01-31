ba0b43dc2bd5f6b5fc2bff8f8e3f6fdbfbe5547c
fuzy <xjtumsu@gmail.com>
2019-01-31 15:18:28 +0800

    改进优化方法

--- mrtrpo_l2/optim.py
+++ mrtrpo_l2/optim.py
@@ -1,97 +1,131 @@
 import numpy as np
 #from pylab import *
 import math
-from cvxpy import *
-from qcqp import *
+#from cvxpy import *
+#from qcqp import *
 import pdb
+
 def get_coefficient(G,S,method = 2):
-    num_reward = G.shape[0]
-    coe = np.ones((num_reward))
-    H = np.dot(S, G.T)
-    SS = np.dot(S, S.T)
-    if method == 1:
-        return l1_norm_constraint(H,num_reward)
-    elif method == 2:
-        return l2_norm_constraint(H,num_reward)
-    elif method == 3:
-        return max_update(SS,H,num_reward)
-    else:
+    try:
+        num_reward = G.shape[0]
+        coe = np.ones((num_reward))
+        H = np.dot(S, G.T)
+        SS = np.dot(S, S.T)
+        if method == 1:
+            return l2_norm_constraint_project(H,num_reward)
+        elif method == 2:
+            return l2_norm_constraint_coord(H,num_reward)
+        else:
+            return coe/num_reward
+    except:
+        num_reward = G.shape[0]
+        coe = np.ones((num_reward))
         return coe/num_reward
 
-def l2_norm_constraint(H,num_reward):
-    #print(H)
-    x = Variable(num_reward)
-    #print(H)
-    #vals, vecs = np.linalg.eigh(H)
-    #max_eigenvalue = np.max(vals)
-    #A = (max_eigenvalue + 1)* np.eye(num_reward)
-    objective = Maximize(quad_form(x, H))
-    constraints = [ x >= 0, x.T * x == 1]
-
-    prob = Problem(objective, constraints)
-    qcqp = QCQP(prob)
-
-    # SDR SPECTRAL
-    qcqp.suggest(SPECTRAL)
-    #print("SDR lower bound: %.3f" % qcqp.sdr_bound)
-
-    # Attempt to improve the starting point given by the suggest method
-    f_cd, v_cd = qcqp.improve(COORD_DESCENT)
-    #primal_result = prob.solve()
-    return np.array(x.value).reshape((num_reward))
-def l1_norm_constraint(H,num_reward):
-    vals, vecs = np.linalg.eigh(H)
-    x = vecs[:,-1]
-    sum1 = np.sum(x > 0)
+def l2_norm_constraint_coord(H,num_reward):
+    eig, eigvec = np.linalg.eig(H)
+    index = np.argmax(eig)
+    vec = eigvec[:,index]
+    sum1 = np.sum(vec > 0)
     if sum1 < 0.5:
-        x = -x
-    x = np.maximum(x,0)
-    x = x / np.sum(x)
-    for i in range(num_reward):
-        #print(np.sum(x * np.dot(H,x)))
-        index1 = np.argmax(x)
-        x[index1] = -1
-        index2 = np.argmax(x)
-        x[index2] = 0
-        lower = 0
-        upper = - np.sum(x)
-        a = H[index1,index1] + H[index2,index2] - H[index1,index2] - H[index2,index1]
-        b = upper * (-2 * H[index2,index2] + H[index1,index2] + H[index2,index1])
-        if a < 1e-6:
-            op = 0 if b < 0 else upper
-        else:
-            middle = b/(-2 * a)
-            if middle < upper/2:
-        	    op = upper
-            else:
-        	    op = 0
-        x[index1] = op
-        x[index2] = upper - op
-    return x
+        vec = -vec
+    if np.sum(vec >= 0) == num_reward:
+        return vec
+    else:
+        vec = np.maximum(vec,0)
+        vec = vec / np.linalg.norm(vec,2)
+        initialobj = np.dot(np.dot(H,vec),vec)
+        alpha = 0.01
+        direct = - np.ones((num_reward,num_reward)) / (num_reward - 1)
+        for i in range(num_reward):
+            direct[i,i] = 1
+        record = np.ones((num_reward))
+        for i in range(1000):
+            #print(vec,np.linalg.norm(vec,2))
+            #print('improve',np.dot(np.dot(H,vec),vec),initialobj)
+            index = i % num_reward
+            objold = np.dot(np.dot(H,vec),vec)
+            vec1 = vec + alpha * direct[index,:]
+            vec2 = vec - alpha * direct[index,:]   
+            vec1 = np.maximum(vec1,0)
+            vec2 = np.maximum(vec2,0)
+            vec1 = vec1 / np.linalg.norm(vec1,2)
+            vec2 = vec2 / np.linalg.norm(vec2,2)
 
-def max_update(SS,H,num_reward):
-    x = Variable(num_reward)
-    objective = Minimize(quad_form(x,H))
-    constraints = [ x >= 0,
-              x.T * SS * x == 1]
+            obj1 = np.dot(np.dot(H,vec1),vec1)
+            obj2 = np.dot(np.dot(H,vec2),vec2)
+            if obj1 > objold and obj1 >= obj2:
+                vec = vec1
+            elif obj2 > objold and obj2 >= obj1:
+                vec = vec2
+            objnew = np.dot(np.dot(H,vec),vec)
+            record[index] = objnew-objold
+            if np.sum(record) <= 1e-8:
+                alpha = alpha / 2
+                record = np.ones((num_reward))
+            if alpha < 0.0001:
+                break
+        return vec
 
-    prob = Problem(objective, constraints)
-    qcqp = QCQP(prob)
+def l2_norm_constraint_project(H,num_reward):
+    eig, eigvec = np.linalg.eig(H)
+    index = np.argmax(eig)
+    vec = eigvec[:,index]
+    sum1 = np.sum(vec > 0)
+    if sum1 < 0.5:
+        vec = -vec
+    if np.sum(vec >= 0) == num_reward:
+        return vec
+    else:
+        vec = np.maximum(vec,0)
+        vec = vec / np.linalg.norm(vec,2)
+        initialobj = np.dot(np.dot(H,vec),vec)
+        alpha = 0.01
+        direct = - np.ones((num_reward,num_reward)) / (num_reward - 1)
+        for i in range(num_reward):
+            direct[i,i] = 1
+        record = np.ones((num_reward))
+        for i in range(1000):
+            #print(vec,np.linalg.norm(vec,2))
+            #print('improve',np.dot(np.dot(H,vec),vec),initialobj)
+            index = i % num_reward
+            objold = np.dot(np.dot(H,vec),vec)
+            direct = np.dot(H,vec)
+            vec1 = vec + alpha * direct
+            vec2 = vec - alpha * direct
+            vec1 = np.maximum(vec1,0)
+            vec2 = np.maximum(vec2,0)
+            vec1 = vec1 / np.linalg.norm(vec1,2)
+            vec2 = vec2 / np.linalg.norm(vec2,2)
 
-    # SDR SPECTRAL
-    qcqp.suggest(SDR)
-    #print("SDR lower bound: %.3f" % qcqp.sdr_bound)
-
-    # Attempt to improve the starting point given by the suggest method
-    f_cd, v_cd = qcqp.improve(COORD_DESCENT)
-
-    return np.array(x.value).reshape((num_reward))
+            obj1 = np.dot(np.dot(H,vec1),vec1)
+            obj2 = np.dot(np.dot(H,vec2),vec2)
+            if obj1 > objold and obj1 >= obj2:
+                vec = vec1
+            elif obj2 > objold and obj2 >= obj1:
+                vec = vec2
+            objnew = np.dot(np.dot(H,vec),vec)
+            record[index] = objnew-objold
+            if np.sum(record) <= 1e-8:
+                alpha = alpha / 2
+                record = np.ones((num_reward))
+            if alpha < 0.0001:
+                break
+        return vec
 
 def test_get_coe():
     n = 3
     A = np.random.rand(n,n)
     H = np.dot(A,A.transpose())
-    coe = l1_norm_constraint(H,n)
+    coe = l2_norm_constraint(H,n)
+    eig, eigvec = np.linalg.eig(H)
+    index = np.argmax(eig)
+    vec = eigvec[index]
+    vec = np.abs(vec)
+    vec = vec / np.linalg.norm(vec,2)
+    print(vec)
+    print(np.dot(np.dot(H,vec),vec))
+    print(np.dot(np.dot(H,coe),coe))
     return coe
 
 
@@ -99,3 +133,4 @@ if __name__ == '__main__':
     for i in range(100):
         coe = test_get_coe()
         print(coe)
+        print("======================================")
--- mrtrpo_mu/optim.py
+++ mrtrpo_mu/optim.py
@@ -1,115 +1,127 @@
 import numpy as np
 #from pylab import *
 import math
-from cvxpy import *
-from qcqp import *
+import scipy.linalg
+#from cvxpy import *
+#from qcqp import *
 import pdb
-def get_coefficient(G,S,method = 3):
-    num_reward = G.shape[0]
-    coe = np.ones((num_reward))
-    H = np.dot(S, G.T)
-    SS = np.dot(S, S.T)
-    if method == 1:
-        return l1_norm_constraint(H,num_reward)
-    elif method == 2:
-        return l2_norm_constraint(H,num_reward)
-    elif method == 3:
-        return max_update(SS,H,num_reward)
-    else:
+def get_coefficient(G,S,method = 2):
+    try:
+        num_reward = G.shape[0]
+        coe = np.ones((num_reward))
+        H = np.dot(S, G.T)
+        SS = np.dot(S, S.T)
+        if method == 1:
+            return max_update_project(SS,H,num_reward)
+        elif method == 2:
+            return max_update_corrd(SS,H,num_reward)
+        else:
+            return coe/num_reward
+    except:
+        num_reward = G.shape[0]
+        coe = np.ones((num_reward))
         return coe/num_reward
 
-def l2_norm_constraint(H,num_reward):
-    #print(H)
-    x = Variable(num_reward)
-    #print(H)
-    #vals, vecs = np.linalg.eigh(H)
-    #max_eigenvalue = np.max(vals)
-    #A = (max_eigenvalue + 1)* np.eye(num_reward)
-    objective = Maximize(quad_form(x, H))
-    constraints = [ x >= 0, x.T * x == 1]
-
-    prob = Problem(objective, constraints)
-    qcqp = QCQP(prob)
-
-    # SDR SPECTRAL
-    qcqp.suggest(SPECTRAL)
-    #print("SDR lower bound: %.3f" % qcqp.sdr_bound)
-
-    # Attempt to improve the starting point given by the suggest method
-    f_cd, v_cd = qcqp.improve(COORD_DESCENT)
-    #primal_result = prob.solve()
-    return np.array(x.value).reshape((num_reward))
-def l1_norm_constraint(H,num_reward):
-    vals, vecs = np.linalg.eigh(H)
-    x = vecs[:,-1]
-    sum1 = np.sum(x > 0)
+def max_update_corrd(SS,H,num_reward):
+    if np.abs(np.linalg.det(SS)) < 1e-6:
+        SS = SS + 0.001 * np.eye(num_reward)
+    A = scipy.linalg.sqrtm(np.linalg.inv(SS))
+    modify_H = np.dot(np.dot(A.T,H),A)
+    eig, eigvec = np.linalg.eig(modify_H)
+    index = np.argmax(eig)
+    vec = eigvec[:,index]
+    vec = np.dot(A,vec)
+    sum1 = np.sum(vec > 0)
     if sum1 < 0.5:
-        x = -x
-    x = np.maximum(x,0)
-    x = x / np.sum(x)
-    for i in range(num_reward):
-        #print(np.sum(x * np.dot(H,x)))
-        index1 = np.argmax(x)
-        x[index1] = -1
-        index2 = np.argmax(x)
-        x[index2] = 0
-        lower = 0
-        upper = - np.sum(x)
-        a = H[index1,index1] + H[index2,index2] - H[index1,index2] - H[index2,index1]
-        b = upper * (-2 * H[index2,index2] + H[index1,index2] + H[index2,index1])
-        if a < 1e-6:
-            op = 0 if b < 0 else upper
-        else:
-            middle = b/(-2 * a)
-            if middle < upper/2:
-        	    op = upper
-            else:
-        	    op = 0
-        x[index1] = op
-        x[index2] = upper - op
-    return x
+        vec = -vec
+    if np.sum(vec >= 0) == num_reward:
+        return vec / np.linalg.norm(vec,2)
+    else:
+        vec = np.maximum(vec,0)
+        vec = vec / m_norm(SS,vec)
+        initialobj = np.dot(np.dot(H,vec),vec)
+        alpha = 0.01
+        direct = - np.ones((num_reward,num_reward)) / (num_reward - 1)
+        for i in range(num_reward):
+            direct[i,i] = 1
+        record = np.ones((num_reward))
+        for i in range(1000):
+            #print(vec,np.linalg.norm(vec,2))
+            #print('improve',np.dot(np.dot(H,vec),vec),initialobj)
+            index = i % num_reward
+            objold = np.dot(np.dot(H,vec),vec)
+            vec1 = vec + alpha * direct[index,:]
+            vec2 = vec - alpha * direct[index,:]   
+            vec1 = np.maximum(vec1,0)
+            vec2 = np.maximum(vec2,0)
+            vec1 = vec1 / m_norm(SS,vec1)
+            vec2 = vec2 / m_norm(SS,vec2)
 
-def max_update(SS,H,num_reward):
-    try:
-        x = Variable(num_reward)
-        objective = Maximize(quad_form(x,H))
-        constraints = [ x >= 0,
-                  x.T * SS * x == 1]
+            obj1 = np.dot(np.dot(H,vec1),vec1)
+            obj2 = np.dot(np.dot(H,vec2),vec2)
+            if obj1 > objold and obj1 >= obj2:
+                vec = vec1
+            elif obj2 > objold and obj2 >= obj1:
+                vec = vec2
+            objnew = np.dot(np.dot(H,vec),vec)
+            record[index] = objnew-objold
+            if np.sum(record) <= 1e-8:
+                alpha = alpha / 2
+                record = np.ones((num_reward))
+            if alpha < 0.0001:
+                break
+        vec = vec / np.linalg.norm(vec,2)
+        return vec
+def m_norm(M,x):
+    return np.sqrt(np.dot(np.dot(M,x),x))
+def max_update_project(SS,H,num_reward):
+    if np.abs(np.linalg.det(SS)) < 1e-6:
+        SS = SS + 0.001 * np.eye(num_reward)
+    A = scipy.linalg.sqrtm(np.linalg.inv(SS))
+    modify_H = np.dot(np.dot(A.T,H),A)
+    eig, eigvec = np.linalg.eig(modify_H)
+    index = np.argmax(eig)
+    vec = eigvec[:,index]
+    vec = np.dot(A,vec)
+    sum1 = np.sum(vec > 0)
+    if sum1 < 0.5:
+        vec = -vec
+    if np.sum(vec >= 0) == num_reward:
+        return vec / np.linalg.norm(vec,2)
+    else:
+        vec = np.maximum(vec,0)
+        vec = vec / m_norm(SS,vec)
+        initialobj = np.dot(np.dot(H,vec),vec)
+        alpha = 0.01
+        record = np.ones((num_reward))
+        for i in range(1000):
+            #print(vec,np.linalg.norm(vec,2))
+            #print('improve',np.dot(np.dot(H,vec),vec),initialobj)
+            index = i % num_reward
+            objold = np.dot(np.dot(H,vec),vec)
+            direct = np.dot(H,vec)
+            vec1 = vec + alpha * direct
+            vec2 = vec - alpha * direct 
+            vec1 = np.maximum(vec1,0)
+            vec2 = np.maximum(vec2,0)
+            vec1 = vec1 / m_norm(SS,vec1)
+            vec2 = vec2 / m_norm(SS,vec2)
 
-        prob = Problem(objective, constraints)
-        qcqp = QCQP(prob)
-
-        # SDR SPECTRAL
-        qcqp.suggest(SDR)
-        #print("SDR lower bound: %.3f" % qcqp.sdr_bound)
-
-        # Attempt to improve the starting point given by the suggest method
-        f_cd, v_cd = qcqp.improve(COORD_DESCENT)
-        return np.maximum(np.array(x.value),0).reshape((num_reward))
-
-    except:
-        try:
-            x = Variable(num_reward)
-            objective = Minimize(quad_form(x,SS))
-            constraints = [ x >= 0,
-                      x.T * H * x == 1]
-
-            prob = Problem(objective, constraints)
-            qcqp = QCQP(prob)
-
-            # SDR SPECTRAL
-            qcqp.suggest(SDR)
-            #print("SDR lower bound: %.3f" % qcqp.sdr_bound)
-
-            # Attempt to improve the starting point given by the suggest method
-            f_cd, v_cd = qcqp.improve(COORD_DESCENT)
-            return np.maximum(np.array(x.value),0).reshape((num_reward))
-        except:
-            nrom_g = np.diag(SS) / np.diag(H)
-            index = np.argmax(nrom_g)
-            res = np.zeros((num_reward))
-            res[index] = 1
-            return res
+            obj1 = np.dot(np.dot(H,vec1),vec1)
+            obj2 = np.dot(np.dot(H,vec2),vec2)
+            if obj1 > objold and obj1 >= obj2:
+                vec = vec1
+            elif obj2 > objold and obj2 >= obj1:
+                vec = vec2
+            objnew = np.dot(np.dot(H,vec),vec)
+            record[index] = objnew-objold
+            if np.sum(record) <= 1e-8:
+                alpha = alpha / 2
+                record = np.ones((num_reward))
+            if alpha < 0.0001:
+                break
+        vec = vec / np.linalg.norm(vec,2)
+        return vec
 
 def test_get_coe():
     n = 3
